bae46986adc4110f7cede24cdb3285fcf8f2bce9
Daniel Imfeld <daniel@imfeld.dev>
2021-03-09 20:33:20 +0000

    Add "replace" handlebars helper

--- src/template.rs
+++ src/template.rs
@@ -18,33 +18,35 @@ handlebars_helper!(join: |list: array, sep: str| {
 });
 
 handlebars_helper!(format_time: |fmt:str, t: i64| { chrono::Utc.timestamp(t / 1000, 0).format(fmt).to_string() });
+handlebars_helper!(replace: |content:str, pattern: str, replacement:str | content.replace(pattern, replacement) );
 
 pub fn create(path: &Path) -> Result<Handlebars> {
-  let mut template_file = match File::open(path) {
-    Ok(f) => f,
-    Err(e) => {
-      if path.is_absolute() {
-        return Err(e.into());
-      }
+    let mut template_file = match File::open(path) {
+        Ok(f) => f,
+        Err(e) => {
+            if path.is_absolute() {
+                return Err(e.into());
+            }
 
-      // Try opening the file under `template/{path}`. If that fails, return the
-      // original error.
-      let template_dir_path = Path::new("templates").join(path);
-      File::open(template_dir_path)
-        .map_err(|_| e)
-        .context("Opening template file")?
-    }
-  };
+            // Try opening the file under `template/{path}`. If that fails, return the
+            // original error.
+            let template_dir_path = Path::new("templates").join(path);
+            File::open(template_dir_path)
+                .map_err(|_| e)
+                .context("Opening template file")?
+        }
+    };
 
-  let mut template = String::new();
-  template_file.read_to_string(&mut template)?;
-  drop(template_file);
+    let mut template = String::new();
+    template_file.read_to_string(&mut template)?;
+    drop(template_file);
 
-  let mut hbars = handlebars::Handlebars::new();
-  hbars.register_template_string("page", template)?;
+    let mut hbars = handlebars::Handlebars::new();
+    hbars.register_template_string("page", template)?;
 
-  hbars.register_helper("join", Box::new(join));
-  hbars.register_helper("format_time", Box::new(format_time));
+    hbars.register_helper("join", Box::new(join));
+    hbars.register_helper("format_time", Box::new(format_time));
+    hbars.register_helper("replace", Box::new(replace));
 
-  Ok(hbars)
+    Ok(hbars)
 }
